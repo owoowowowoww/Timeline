dfc7ebcf1ed807bea8514206e37f031b5fea9283
Maxence Lissonnet <maxencel86@outlook.fr>
2025-05-27 14:51:13 +0200

    Creation page Parametre Partie

---
+++ src/main/java/com/example/timeline/ParametrePartieController.java
@@ -0,0 +1,12 @@
+package com.example.timeline;
+
+import javafx.stage.Stage;
+
+public class ParametrePartieController {
+    Stage mainStage;
+
+    public void setStage(Stage stage) {
+        this.mainStage = stage;
+    }
+
+}
