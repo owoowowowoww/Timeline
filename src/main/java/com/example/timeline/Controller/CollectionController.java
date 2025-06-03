acc15449ec58492287ce6a9c4b2d5b85ac5e184d
Maxence Lissonnet <maxencel86@outlook.fr>
2025-06-03 09:47:06 +0200

    Ajout du bouton retour sur l'ensemble des page

--- src/main/java/com/example/timeline/Controller/CollectionController.java
+++ src/main/java/com/example/timeline/Controller/CollectionController.java
@@ -1,6 +1,13 @@
 package com.example.timeline.Controller;
 
+import com.example.timeline.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CollectionController {
     Stage mainStage;
@@ -9,4 +16,13 @@ public class CollectionController {
         this.mainStage = stage;
     }
 
+    @FXML
+    void onClickRetour(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Acceuil.fxml"));
+        Scene acceuilScene = new Scene(fxmlLoader.load(), 950, 635);
+        mainStage.setTitle("Acceuil");
+        mainStage.setScene(acceuilScene);
+        AcceuilController controller = fxmlLoader.getController();
+        controller.setStage(mainStage);
+    }
 }
--- src/main/java/com/example/timeline/Controller/ParametreController.java
+++ src/main/java/com/example/timeline/Controller/ParametreController.java
@@ -1,11 +1,28 @@
 package com.example.timeline.Controller;
 
+import com.example.timeline.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ParametreController {
     Stage mainStage;
 
     public void setStage(Stage stage) {
         this.mainStage = stage;
+    }
+
+    @FXML
+    void onClickRetour(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Acceuil.fxml"));
+        Scene acceuilScene = new Scene(fxmlLoader.load(), 950, 635);
+        mainStage.setTitle("Acceuil");
+        mainStage.setScene(acceuilScene);
+        AcceuilController controller = fxmlLoader.getController();
+        controller.setStage(mainStage);
     }
 }
