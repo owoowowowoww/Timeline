748d3368e327664f8e056471ac67a1cf0334b5bd
Igleace <103002857+Igleace@users.noreply.github.com>
2025-06-03 10:06:29 +0200

    Initialisation des variables pour démarrer la partie

--- src/main/java/com/example/timeline/board/Timeline.java
+++ src/main/java/com/example/timeline/board/Timeline.java
@@ -1,11 +1,34 @@
 package com.example.timeline.board;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import com.example.timeline.collection.*;
+
 public class Timeline {
-    static void initGame(){
 
-    }
+    private List<Player> players;
+    private PileOfCards drawPile;
+    private List<Card> board;
+    private int currentPlayerIndex;
+    private boolean gameEnded;
 
-    public static void main(String[] args) {
-        initGame();
+    public void Game(List<Player> players, List<Card> allCards, int cardsPerPlayer) {
+        this.players = players;
+        this.drawPile = new PileOfCards();
+        this.board = new ArrayList<>();
+        this.currentPlayerIndex = 0;
+        this.gameEnded = false;
+
+        Collections.shuffle(allCards);
+        for (int i = 0; i < cardsPerPlayer; i++) {
+            for (Player player : players) {
+                player.getHand().receiveCard(allCards.removeFirst());
+            }
+        }
+
+        for (Card card : allCards) {
+            drawPile.receiveCard(card);
+        }
     }
 }
